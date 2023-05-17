3eb1c368789ae9a1782d212109d2218e801611fb
MihajaUser <81333217+MihajaUser@users.noreply.github.com>
2023-05-17 12:34:45 +0300

    v2 panier

--- views/Main.java
+++ views/Main.java
@@ -42,26 +42,33 @@ public class Main extends JFrame {
       );
     ///PARTIE GAUCHE /////////////////////////////////////////////////////////////
     // Ajouter un bouton et une zone de saisie à chaque panneau
-    JButton boutonGauche = new JButton("Bouton Gauche");
-    boutonGauche.setBounds(250, 400, 180, 25);
-    JTextField saisieGauche = new JTextField(20);
-    panelGauche.add(boutonGauche);
-    panelGauche.add(saisieGauche);
-
+    JLabel nomProduit = new JLabel(" Redmin note 7");
+    JLabel prixProduit = new JLabel("100 Euro");
+    nomProduit.setBounds(100, 10, 200, 100);
+    prixProduit.setBounds(450, 10, 200, 100);
+    panelGauche.add(nomProduit);
+    panelGauche.add(prixProduit);
     ///PARTIE DROIT/////////////////////////////////////////////////////////////
     JLabel panier = new JLabel("Panier");
+    JLabel prixPanier =new JLabel("249 Euro");
     JLabel frais = new JLabel("Frais de livraison estimé");
+    JLabel prixFrais =new JLabel("Gratuit");
     JLabel total = new JLabel("Total");
+    JLabel prixTotal = new JLabel("249 Euro");
     JLabel tva = new JLabel("(TVA incluse)");
     JLabel economie = new JLabel("Vous écomisez 30 Eur");
     Font totalFont = total.getFont();
     total.setFont(new Font(totalFont.getName(), Font.BOLD, 20));
     Font economieFont = economie.getFont();
     JButton boutonDroit = new JButton("Valider mon panier");
+    economie.setForeground(Color.GREEN);
     economie.setFont(new Font(economieFont.getName(), Font.BOLD, 14));
     panier.setBounds(10, 40, 100, 100);
+    prixPanier.setBounds(225, 40, 100, 100);
     frais.setBounds(10, 90, 300, 100);
+    prixFrais.setBounds(235, 90, 300, 100);
     total.setBounds(10, 150, 100, 100);
+    prixTotal.setBounds(223, 150, 100, 100);
     tva.setBounds(70, 150, 100, 100);
     economie.setBounds(115, 200, 300, 100);
     boutonDroit.setBounds(40, 315, 230, 35);
@@ -70,8 +77,11 @@ public class Main extends JFrame {
     boutonDroit.setBorder(null);
     panelDroit.add(boutonDroit);
     panelDroit.add(panier);
+    panelDroit.add(prixPanier);
     panelDroit.add(frais);
+    panelDroit.add(prixFrais);
     panelDroit.add(total);
+    panelDroit.add(prixTotal);
     panelDroit.add(tva);
     panelDroit.add(economie);
   }
